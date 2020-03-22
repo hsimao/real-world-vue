9c6558210e60f51d2304a865e605c89460114057
hsimao <e087754958@gmail.com>
2020-03-22 14:01:33 +0800

    feat: use mixins 封裝通用 BaseInput、BaseSelect 屬性、方法

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -10,31 +10,15 @@
 </template>
 
 <script>
+import { formFieldMixin } from '@/mixins/formFieldMixin'
+
 export default {
   name: 'BaseSelect',
-  inheritAttrs: false,
+  mixins: [formFieldMixin],
   props: {
     options: {
       type: Array,
       required: true
-    },
-    label: {
-      type: String,
-      default: ''
-    },
-    value: [String, Number]
-  },
-  computed: {
-    listeners() {
-      return {
-        ...this.$listeners,
-        input: this.updateValue
-      }
-    }
-  },
-  methods: {
-    updateValue(event) {
-      this.$emit('input', event.target.value)
     }
   }
 }
